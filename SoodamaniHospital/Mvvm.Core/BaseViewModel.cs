2fdd6a035bbdedb6f4011bdfba59d7046d562a2c
sundarsofi <syncsundar@live.com>
2016-08-31 22:59:07 +0530

    view model base added.

---
+++ SoodamaniHospital/Mvvm.Core/BaseViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Practices.Prism.PubSubEvents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Windows;
+
+namespace Mvvm.Core
+{
+    public abstract class BaseViewModel : NotifiableObject
+        {
+        private static bool? _isInDesignMode;
+
+        public BaseViewModel(IEventAggregator eventAggregator)
+            :base(eventAggregator)
+        {
+        }
+
+        protected virtual void RaisePropertyChanged<T>(string propertyName, T oldValue = default(T), T newValue = default(T))
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException("This method cannot be called with an empty string", "propertyName");
+            }
+
+            RaisePropertyChanged(propertyName);
+        }
+
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression, T oldValue, T newValue)
+        {
+            var handler = PropertyChangedHandler;
+
+            if (handler != null)
+            {
+                var propertyName = GetPropertyName(propertyExpression);
+
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+
+        protected bool Set<T>(Expression<Func<T>> propertyExpression, ref T field, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            RaisePropertyChanging(propertyExpression);
+            var oldValue = field;
+            field = newValue;
+            RaisePropertyChanged(propertyExpression, oldValue, field);
+            return true;
+        }
+
+        protected bool Set<T>(string propertyName, ref T field, T newValue = default(T))
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            RaisePropertyChanging(propertyName);
+            var oldValue = field;
+            field = newValue;
+            RaisePropertyChanged(propertyName, oldValue, field);
+            return true;
+        }
+    }
+}
---
+++ SoodamaniHospital/Mvvm.Core/NotifiableObject.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Microsoft.Practices.Prism.PubSubEvents;
+
+namespace Mvvm.Core
+{
+    public class NotifiableObject : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        public NotifiableObject(IEventAggregator eventAggregator)
+        {
+            EventAggregator = eventAggregator;
+        }
+
+        public IEventAggregator EventAggregator { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected PropertyChangedEventHandler PropertyChangedHandler
+        {
+            get
+            {
+                return PropertyChanged;
+            }
+        }
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected PropertyChangingEventHandler PropertyChangingHandler
+        {
+            get
+            {
+                return PropertyChanging;
+            }
+        }
+
+        protected virtual void RaisePropertyChanging(string propertyName)
+        {
+            var handler = PropertyChanging;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void RaisePropertyChanging<T>(Expression<Func<T>> propertyExpression)
+        {
+            var handler = PropertyChanging;
+            if (handler != null)
+            {
+                var propertyName = GetPropertyName(propertyExpression);
+                handler(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                var propertyName = GetPropertyName(propertyExpression);
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected static string GetPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+            {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            var body = propertyExpression.Body as MemberExpression;
+
+            if (body == null)
+            {
+                throw new ArgumentException("Invalid argument", "propertyExpression");
+            }
+
+            var property = body.Member as PropertyInfo;
+
+            if (property == null)
+            {
+                throw new ArgumentException("Argument is not a property", "propertyExpression");
+            }
+
+            return property.Name;
+        }
+
+        protected bool Set<T>(Expression<Func<T>> propertyExpression, ref T field, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            RaisePropertyChanging(propertyExpression);
+            field = newValue;
+            RaisePropertyChanged(propertyExpression);
+            return true;
+        }
+
+        protected bool Set<T>(string propertyName, ref T field, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            RaisePropertyChanging(propertyName);
+            field = newValue;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+        
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            return Set(propertyName, ref field, newValue);
+        }
+    }
+}
